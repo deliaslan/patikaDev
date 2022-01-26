d41e8324b2733f9fd9afefbd9447f49d2c6e4ace
Adem Deliaslan <deliaslan@gmail.com>
2022-01-26 12:27:43 +0300

    Swift Değişken ve Sabitlerin Kullanımı
    Swift dersleri değişken ve sabitleme kullanımının öğrenimi

---
+++ Swift/DegiskenVeSabitler/VarAndLet.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var bosluk: String
+
+let pi = 3.14
+
+let bosSabit: Int
+
+var isim = "Adem"
+
+print(greeting + isim) //komut satırına yazar
+print("\(greeting) \(isim)") //komut satırına yazar
+
+
+var yaslar = [3, 5, 7, 9, 15]
+var isimler = ["Ali", "Ahmet", "Adem", "Çiğdem"]
+
+
+print("\(isimler[0]), \(yaslar[4]) yaşındadır")
+
+
