07487ee2e31e41b14a2a01e093585f2f446dc10b
Adem Deliaslan <deliaslan@gmail.com>
2022-01-28 12:51:51 +0300

    Eklediğimiz UITableView üzerinde prototip cell oluşturduk ve verilere göre ekranda oluşacak satırları bu prototipten ürettik.

--- Swift/Projects/ListApp/ListApp/Controllers/ViewController.swift
+++ Swift/Projects/ListApp/ListApp/Controllers/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["Swift", "Kotlin", "C++", "C", "Pytohn", "Java", "C#"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    //datasource için datayı çevirecek 2 metodu eklememiz gerekiyor.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
 
